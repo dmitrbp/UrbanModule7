36bb609aed9ff195d236b48cd1740c77cfa7b31a
Дмитрий <dmitrbp@jac.ru>
2024-07-25 18:05:38 +0300

    release 1

--- Module_7_3.py
+++ Module_7_3.py
@@ -50,7 +50,7 @@ class WordsFinder:
         return result
 
 
-finder2 = WordsFinder('test_file.txt', 'test_file2.txt')
+finder2 = WordsFinder('test_file.txt')
 print(finder2.get_all_words())  # Все слова
 print(finder2.find('TEXT'))  # 3 слово по счёту
 print(finder2.count('teXT'))  # 4 слова teXT в тексте всего
